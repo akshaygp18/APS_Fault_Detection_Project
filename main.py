7788f58ff039db6715777333dfbe9525b5dc9a86
akshaygp18 <akshaygp41@gmail.com>
2022-12-24 22:36:39 +0530

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sensor.pipeline.training_pipline import start_training_pipeline
+from sensor.pipeline.training_pipeline import start_training_pipeline
 from sensor.pipeline.batch_prediction import start_batch_prediction
 
 file_path = "/config/workspace/aps_failure_training_set1.csv"
